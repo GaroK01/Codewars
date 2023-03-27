c242e7dfca81d67e1a6e9ac84317f9f4361108ce
Garo Kalfayan <64748704+GaroK01@users.noreply.github.com>
2023-03-27 22:06:53 +0300

    Create Even or Odd.java

---
+++ Solutions/Java/Even or Odd.java
@@ -0,0 +1,9 @@
+public class EvenOrOdd {
+    public static String even_or_odd(int number) {
+        //Place code here
+        if(number%2==0){
+          return "Even";
+        }
+      return "Odd";
+    }
+}
