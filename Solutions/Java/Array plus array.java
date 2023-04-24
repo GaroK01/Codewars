b95c0d83eb71855d12b2cb9b404fa0a6af634745
Garo Kalfayan <64748704+GaroK01@users.noreply.github.com>
2023-04-24 18:23:16 +0300

    Update Array plus array.java

--- Solutions/Java/Array plus array.java
+++ Solutions/Java/Array plus array.java
@@ -1,1 +1,18 @@
+public class Sum {
 
+  public static int arrayPlusArray(int[] arr1, int[] arr2) {
+    // arr1 + arr2 is not working...
+    int n=0;
+    int n1=0;
+    int m;
+    for(int i=0;i<arr1.length;i++){
+      n=n+arr1[i];
+    }
+    
+    for(int i1=0;i1<arr2.length;i1++){
+      n1=n1+arr2[i1];
+    }
+    m=n1+n;
+    return m;
+//     return n;
+  }
