66042fe726a0f21784a45f3057fc01e1e9715350
Garo Kalfayan <64748704+GaroK01@users.noreply.github.com>
2023-04-24 18:19:26 +0300

    Create Transportation on vacation.java

---
+++ Solutions/Java/Transportation on vacation.java
@@ -0,0 +1,12 @@
+public class Kata {
+  public static int rentalCarCost(int d) {
+    // Your solution here
+    if(d>=7){
+      return (d*40)-50;
+    }
+    else if(d>=3){
+      return (d*40)-20;
+    }
+    return d*40;
+  }
+}
