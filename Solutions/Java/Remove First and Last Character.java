73af0c70e59b09b16fa229382dc3ecca31db700c
Garo Kalfayan <64748704+GaroK01@users.noreply.github.com>
2023-04-24 18:20:57 +0300

    Create Remove First and Last Character.java

---
+++ Solutions/Java/Remove First and Last Character.java
@@ -0,0 +1,8 @@
+public class RemoveChars {
+    public static String remove(String str) {
+       
+        // your code here
+      int n = str.length();
+      return str.substring(1,n-1);
+    }
+}
