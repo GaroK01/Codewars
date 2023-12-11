a3d5739dd216ef5035a0a9e2b21d17870b13e162
Garo Kalfayan <64748704+GaroK01@users.noreply.github.com>
2023-12-11 22:41:42 +0200

    Create Keep up the hoop.java

---
+++ Solutions/Java/Keep up the hoop.java
@@ -0,0 +1,8 @@
+ public class HelpAlex{
+  public static String hoopCount(int n){
+    if(n>=10){
+      return "Great, now move on to tricks";
+    }
+   return "Keep at it until you get it";
+  }
+}
