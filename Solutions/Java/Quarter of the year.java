62fa9e1fd35eaee78d53566ddaef7ff4f32068d5
Garo Kalfayan <64748704+GaroK01@users.noreply.github.com>
2023-12-11 22:43:51 +0200

    Create Quarter of the year.java

---
+++ Solutions/Java/Quarter of the year.java
@@ -0,0 +1,15 @@
+ public class Kata {
+    public static int quarterOf(int month) {
+        // Your code here
+      if(month>=1 && month<=3){
+        return 1;
+      }
+      else  if(month>=4 && month<=6){
+        return 2;
+      }
+      else  if(month>=7 && month<=9){
+        return 3;
+      }
+      return 4;
+    }
+}
