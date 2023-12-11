9d673544ffdc5a1e5411ed40c6b1952fc6859b84
Garo Kalfayan <64748704+GaroK01@users.noreply.github.com>
2023-12-11 22:37:34 +0200

    Create Are You Playing Banjo?.java

---
+++ Solutions/Java/Are You Playing Banjo?.java
@@ -0,0 +1,9 @@
+public class Banjo {
+  public static String areYouPlayingBanjo(String name) {
+    // Program me!
+    if(name.startsWith("R")||name.startsWith("r")){
+      return name + " plays banjo";
+    }
+    return name + " does not play banjo";
+  }
+}
