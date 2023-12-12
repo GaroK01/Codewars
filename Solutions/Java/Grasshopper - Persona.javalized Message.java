ed53216bba40b34053dc4128f2d191297a93ce82
Garo Kalfayan <64748704+GaroK01@users.noreply.github.com>
2023-12-12 20:49:48 +0200

    Create Grasshopper - Persona.javalized Message.java

---
+++ Solutions/Java/Grasshopper - Persona.javalized Message.java
@@ -0,0 +1,9 @@
+ class Kata {
+    static String greet(String name, String owner) {
+        // Add code here
+      if(name.equals(owner)){
+        return "Hello boss";
+      }
+      return "Hello guest";
+    }
+}
