6552e8543933ae1afec973f2f50a15a491a85374
Garo Kalfayan <64748704+GaroK01@users.noreply.github.com>
2023-12-13 20:30:42 +0200

    Create Count the Monkeys!.java

---
+++ Solutions/Java/Count the Monkeys!.java
@@ -0,0 +1,10 @@
+ public class MonkeyCounter
+{
+  public static int[] monkeyCount(final int n){
+    int[] arr=new int[n];
+    for(int i=0;i<n;i++){
+      arr[i]=i+1;
+    }
+    return arr;
+  }
+}
