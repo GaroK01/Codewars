de2238a58ad9727467a98aa03281be00911aed4b
Garo Kalfayan <64748704+GaroK01@users.noreply.github.com>
2023-12-13 20:26:02 +0200

    Create String repeat.java

---
+++ Solutions/Java/String repeat.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public static String repeatStr(final int repeat, final String string) {
+      String a="";
+      for(int i=0;i<repeat;i++){
+        a+=string;
+      }
+      return a;
+    }
+}
