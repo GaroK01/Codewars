f8c5aafa642c58317576c5561d77c1c8be7e770f
Garo Kalfayan <64748704+GaroK01@users.noreply.github.com>
2023-12-13 20:32:03 +0200

    Create Determine offspring sex based on genes XX and XY chromosomes.java

---
+++ Solutions/Java/Determine offspring sex based on genes XX and XY chromosomes.java
@@ -0,0 +1,11 @@
+ public class Kata {
+  public static String chromosomeCheck(String sperm) {
+    // Your code goes here. Have fun!
+      if(sperm=="XY"){
+    return"Congratulations! You're going to have a son.";
+  }
+  else{
+    return "Congratulations! You're going to have a daughter.";
+  }
+  }
+}
