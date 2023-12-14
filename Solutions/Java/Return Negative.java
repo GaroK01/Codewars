361c8498b5b3e0d7d9579813b62dc3639f7cc4e2
Garo Kalfayan <64748704+GaroK01@users.noreply.github.com>
2023-12-14 21:23:24 +0200

    Create Return Negative.java

---
+++ Solutions/Java/Return Negative.java
@@ -0,0 +1,16 @@
+ public class Kata {
+
+  public static int makeNegative(final int x) {
+    if (x == 0){
+     return 0;  
+    }
+    else if (x < 0){
+      return x ;
+    }
+    else {
+      return x * -1;
+    }
+    
+  }
+  
+}
