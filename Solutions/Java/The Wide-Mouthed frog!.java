1498d808d928e81edd623ed931f8f4b00c8d591b
Garo Kalfayan <64748704+GaroK01@users.noreply.github.com>
2023-12-17 22:25:35 +0200

    Create The Wide-Mouthed frog!.java

---
+++ Solutions/Java/The Wide-Mouthed frog!.java
@@ -0,0 +1,10 @@
+public class WideMouthedFrog{
+  public static String mouthSize(String animal){
+   if (animal.equalsIgnoreCase("alligator")){
+     return "small";
+   }
+    else{
+      return "wide";
+    }
+  }
+}
