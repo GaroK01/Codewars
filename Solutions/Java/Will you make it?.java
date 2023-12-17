ad02718e309d60526a921f262dc59d3d34c88b71
Garo Kalfayan <64748704+GaroK01@users.noreply.github.com>
2023-12-17 22:26:20 +0200

    Create Will you make it?.java

---
+++ Solutions/Java/Will you make it?.java
@@ -0,0 +1,13 @@
+ public class Kata {
+  
+  public static boolean zeroFuel(double distanceToPump, double mpg, double fuelLeft) {
+    
+    if (fuelLeft * mpg >= distanceToPump ){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+  
+}
