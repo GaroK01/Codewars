ca7fdb2ee40ecae19a4a8a8c2407da091f812d15
Garo Kalfayan <64748704+GaroK01@users.noreply.github.com>
2023-12-24 23:29:44 +0200

    Create Filtering even numbers (Bug Fixes).java

---
+++ Solutions/Java/Filtering even numbers (Bug Fixes).java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public class Kata13December {
+    public static List<Integer> filterOddNumber(List<Integer> listOfNumbers)
+    {
+        for (int i = 0; i < listOfNumbers.size(); i++)
+        {
+            if (listOfNumbers.get(i)%2 == 0)
+            {
+                listOfNumbers.remove(i);
+                i--;
+            }
+         
+        }
+       return listOfNumbers;
+    }
+}
